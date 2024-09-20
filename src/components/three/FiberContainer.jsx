2a0ac91d7836680ad4400117ace67a688f02cf67
treemanw1 <wee.neville@ymail.com>
2024-09-20 21:46:44 +0800

    chore: add responsiveness for mobile view
    Only added for desktop and mobile view so far. Haven't tested for anything
    other than the default iphone format in chrome web tools.

--- src/components/three/FiberContainer.jsx
+++ src/components/three/FiberContainer.jsx
@@ -2,9 +2,9 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Scene } from "./Scene";
 
-export function FiberContainer() {
+export function FiberContainer({cameraPos}) {
   return (
-    <Canvas camera={{ position: [9.9666, 3.0365, 6.556165], fov: 35 }} shadows>
+    <Canvas camera={{ position: [cameraPos.x, cameraPos.y, cameraPos.z], fov: 35 }} shadows>
       <Scene />
       {/* <OrbitControls minDistance={1} maxDistance={200} /> */}
     </Canvas>
